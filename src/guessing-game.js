e3c46dc3035cc5d6087d2205174bb1df3c9be453
ViktoriaKondrashova1 <kavidmi@gmail.com>
2022-08-16 18:14:40 +0300

    feat: finish guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
+let guessNum = 0;
+
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return guessNum = (Math.round((this.maxValue + this.minValue) / 2));
     }
 
     lower() {
-
+        return this.maxValue = guessNum;
     }
 
     greater() {
-
+        return this.minValue = guessNum;
     }
-}
+};
+
+console.log(guessNum)
 
 module.exports = GuessingGame;
+
